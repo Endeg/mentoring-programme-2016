32cf323cbce3d6fc483ea10d30da3ad3c641b128
Andrey Gladyshev <vodomerkin@gmail.com>
2016-08-26 15:18:08 +0600

    Alternative storage path for files.

--- Multithreading/currency/src/main/java/com/epam/mentoring/multithreading/currency/CurrencyRunner.java
+++ Multithreading/currency/src/main/java/com/epam/mentoring/multithreading/currency/CurrencyRunner.java
@@ -1,11 +1,13 @@
 package com.epam.mentoring.multithreading.currency;
 
+import java.nio.file.Paths;
+
 /**
  * Created by Endeg on 26.08.2016.
  */
 public class CurrencyRunner {
 
     public static void main(String[] args) {
-
+        System.out.println(Paths.get(".").toAbsolutePath().normalize().toString());
     }
 }
--- Multithreading/currency/src/main/java/com/epam/mentoring/multithreading/currency/dao/AbstractGsonReadOnlyDao.java
+++ Multithreading/currency/src/main/java/com/epam/mentoring/multithreading/currency/dao/AbstractGsonReadOnlyDao.java
@@ -5,19 +5,34 @@ import com.google.gson.Gson;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Paths;
 
 /**
  * Created by Endeg on 26.08.2016.
  */
 public abstract class AbstractGsonReadOnlyDao<E, PK> implements ReadOnlyDao<E, PK> {
+
+    public static final String STORAGE_PATH = Paths.get(".").toAbsolutePath().normalize().toString();
+
     public E get(PK id) throws DaoException {
         final Gson gson = new Gson();
-        final InputStream jsonStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(id + ".json");
+
+        final String filePath = id + ".json";
+        final String storagePath = Paths.get(STORAGE_PATH, filePath).normalize().toString();
+
+        InputStream jsonStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(storagePath);
+        if (jsonStream == null) {
+            jsonStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
+        }
         if (jsonStream != null) {
             return gson.fromJson(new InputStreamReader(jsonStream), getEntityClass());
         } else {
             throw new DaoException("Entity of " + getEntityClass() + " with id " + id + " was not found");
         }
+    }
+
+    public void save(PK id, E entity) {
+        //final Gson gson = new Gson();
     }
 
     protected abstract Class<E> getEntityClass();
