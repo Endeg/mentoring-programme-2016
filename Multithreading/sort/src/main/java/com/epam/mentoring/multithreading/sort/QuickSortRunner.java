b958117d906f609ec69c1f28e3bb340b5407aa0d
Andrey Gladyshev <vodomerkin@gmail.com>
2016-08-26 18:37:07 +0600

    Single thread implementation of qsort.

--- Multithreading/sort/src/main/java/com/epam/mentoring/multithreading/sort/QuickSortRunner.java
+++ Multithreading/sort/src/main/java/com/epam/mentoring/multithreading/sort/QuickSortRunner.java
@@ -1,5 +1,9 @@
 package com.epam.mentoring.multithreading.sort;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -11,10 +15,56 @@ public class QuickSortRunner {
 
     public static void main(String[] args) {
         final Random rnd = new Random();
-        List<Integer> unsorted = new ArrayList<Integer>();
-        for (int i = 0; i < 100000; i++) {
-            unsorted.add(rnd.nextInt());
+        List<Integer> raw = new ArrayList<Integer>();
+        for (int i = 0; i < 1000; i++) {
+            raw.add(rnd.nextInt());
+        }
+
+        final ImmutableList<Integer> unsorted = ImmutableList.copyOf(raw);
+
+        final ImmutableList<Integer> sorted = qsort(unsorted);
+
+        for (Integer elem : sorted) {
+            System.out.println(elem);
         }
     }
 
+    private static ImmutableList<Integer> qsort(ImmutableList<Integer> unsorted) {
+        if (unsorted.isEmpty()) {
+            return unsorted;
+        }
+
+        if (unsorted.size() == 1) {
+            return unsorted;
+        }
+
+        final Integer x = unsorted.get(0);
+        final ImmutableList<Integer> xs = unsorted.subList(1, unsorted.size() - 1);
+
+        return ImmutableList.<Integer>builder().
+                addAll(qsort(filterMinElements(xs, x))).
+                add(x).
+                addAll(qsort(filterMaxElements(xs, x))).
+                build();
+    }
+
+    private static <E extends Comparable<E>> ImmutableList<E> filterMinElements(ImmutableList<E> xs, final E x) {
+        final Iterable<E> filtered = Iterables.filter(xs, new Predicate<E>() {
+            public boolean apply(E e) {
+                return e.compareTo(x) < 0;
+            }
+        });
+
+        return ImmutableList.copyOf(filtered);
+    }
+
+    private static <E extends Comparable<E>> ImmutableList<E> filterMaxElements(ImmutableList<E> xs, final E x) {
+        final Iterable<E> filtered = Iterables.filter(xs, new Predicate<E>() {
+            public boolean apply(E e) {
+                return e.compareTo(x) >= 0;
+            }
+        });
+
+        return ImmutableList.copyOf(filtered);
+    }
 }
