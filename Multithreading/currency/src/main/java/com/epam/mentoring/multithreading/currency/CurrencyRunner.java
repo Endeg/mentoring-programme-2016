5c08bc5ccc0989db6b2654eb98cfc0d4fedd1fb9
Andrey Gladyshev <vodomerkin@gmail.com>
2016-08-26 13:55:34 +0600

    gitignore and currency runner

---
+++ Multithreading/currency/src/main/java/com/epam/mentoring/multithreading/currency/CurrencyRunner.java
@@ -0,0 +1,11 @@
+package com.epam.mentoring.multithreading.currency;
+
+/**
+ * Created by Endeg on 26.08.2016.
+ */
+public class CurrencyRunner {
+
+    public static void main(String[] args) {
+
+    }
+}
