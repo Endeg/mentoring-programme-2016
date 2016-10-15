9af781a2de8faf17d90a2b183f7cfb7809d1b33a
Andrey Gladyshev <vodomerkin@gmail.com>
2016-10-16 00:58:58 +0600

    Maven-Graddle HW - tomcat deploy.

---
+++ Maven-Graddle/web/src/main/java/com/epam/mentoring/app/web/MainConfiguration.java
@@ -0,0 +1,14 @@
+package com.epam.mentoring.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Endeg on 16.10.2016.
+ */
+@SpringBootApplication
+public class MainConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(MainConfiguration.class, args);
+    }
+}
---
+++ Maven-Graddle/web/src/main/java/com/epam/mentoring/app/web/RandomStringRest.java
@@ -0,0 +1,20 @@
+package com.epam.mentoring.app.web;
+
+/**
+ * Created by Endeg on 15.10.2016.
+ */
+
+import com.epam.mentoring.app.business.RandomStringService;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RandomStringRest {
+
+    private final RandomStringService service = new RandomStringService();
+
+    public List<String> getRandomStrings() {
+        return service.getStrings(16);
+    }
+}
