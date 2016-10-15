889bc234f16cc2902c61058f92f8dd1b40f89699
Andrey Gladyshev <vodomerkin@gmail.com>
2016-10-16 01:03:29 +0600

    Maven-Graddle HW - rest service.

--- Maven-Graddle/web/src/main/java/com/epam/mentoring/app/web/RandomStringRest.java
+++ Maven-Graddle/web/src/main/java/com/epam/mentoring/app/web/RandomStringRest.java
@@ -5,6 +5,7 @@ package com.epam.mentoring.app.web;
  */
 
 import com.epam.mentoring.app.business.RandomStringService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class RandomStringRest {
 
     private final RandomStringService service = new RandomStringService();
 
+    @RequestMapping("/strings")
     public List<String> getRandomStrings() {
         return service.getStrings(16);
     }
